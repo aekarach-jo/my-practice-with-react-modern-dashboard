c35b7d2c292933c2197a58e97dd5b575768d1867
aekarch.jo <aekarach.jomkhamsing@gmail.com>
2022-09-01 20:57:22 +0700

    update

--- src/components/Updates/Updates.jsx
+++ src/components/Updates/Updates.jsx
@@ -5,20 +5,19 @@ import { UpdatesData } from "../../Data/Data";
 const Updates = () => {
   return (
     <div className="Updates">
-      {UpdatesData.map((update) => {
-        return (
-          <div className="update">
-            <img src={update.img} alt="profile" />
-            <div className="noti">
-              <div  style={{marginBottom: '0.5rem'}}>
-                <span>{update.name}</span>
-                <span> {update.noti}</span>
-              </div>
-                <span>{update.time}</span>
+      {UpdatesData.map((update, index) => (
+        <div key={index} className="update">
+          <img src={update.img} alt="profile" />
+          <div className="noti">
+            <div style={{ marginBottom: '0.5rem' }}>
+              <span>{update.name}</span>
+              <span> {update.noti}</span>
             </div>
+            <span>{update.time}</span>
           </div>
-        );
-      })}
+        </div>
+      )
+      )}
     </div>
   );
 };
